ad37e7c42b5d61a68e8c0a7a80f1b8397601bc34
Luis Paulo França <franca.luiz.paulo.777@gmail.com>
2021-11-30 19:14:50 -0300

    Interactive Menu

---
+++ assets/js/main.js
@@ -0,0 +1,4 @@
+const addActiveStatus = () => document.getElementById('menu').classList.add('active')
+
+document.getElementById('home').addEventListener('click', alert('ativado home'))
+document.getElementById('about-us').addEventListener('click', alert('ativado quem somos'))
